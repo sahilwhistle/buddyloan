7ea8e8399c86eb1808f70d78035306ee1aa8f46b
sammeddev <sammed.hunchimare@buddyloan.com>
2024-11-13 18:54:13 +0530

    mod: toggle btn height reduced

--- app/components/common/ToggleButton.tsx
+++ app/components/common/ToggleButton.tsx
@@ -12,12 +12,12 @@ const ToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({
   setSelectedValue,
 }) => {
   return (
-    <div className="mb-6 flex justify-center border-[1px] bg-white border-blue rounded-[30px] border-[#253C82] h-[55px]">
+    <div className="mb-6 flex justify-center border-[1px] bg-white border-blue rounded-[30px] border-[#253C82] h-[45px]">
       {options.map((option, index) => (
         <div
           key={option.value}
           onClick={() => setSelectedValue(option.value)}
-          className={`flex items-center justify-center text-center font-bold w-full cursor-pointer px-4 py-3 rounded-full ${
+          className={`flex items-center justify-center text-center font-bold w-full cursor-pointer rounded-full ${
             selectedValue === option.value
               ? "bg-gradient-to-r from-[#6F89D6] to-[#243B81] text-white"
               : "bg-white text-[#253C82]"
