e7cb03caac9f79b8ed92b7ccfc37226bfb10452a
sammeddev <sammed.hunchimare@buddyloan.com>
2024-11-14 11:23:34 +0530

    add: globally poppins font family added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 export default {
   content: [
@@ -12,6 +13,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
         'email-blue': '#00A6FF', 
+      },
+      fontFamily: {
+        sans: ['var(--font-poppins)', ...fontFamily.sans],
       },
     },
   },
