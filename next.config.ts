ae393c952b267f49ec508cfd9d7fc2fae48134a2
sammeddev <sammed.hunchimare@buddyloan.com>
2024-11-18 18:43:08 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/dsa',
+  assetPrefix: '/dsa',
   /* config options here */
   images: {
     domains: ['via.placeholder.com'], 
